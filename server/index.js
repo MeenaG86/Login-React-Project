ad4ef003d5797499a20e30c8fb59b096d41072c6
MeenaG86 <meeash2021@gmail.com>
2026-04-22 21:36:03 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -38,6 +38,10 @@ app.post("/login", (req, res) => {
   }
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
